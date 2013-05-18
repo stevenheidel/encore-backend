4442b0f84f5534f25653c44e04615d808a9472c9
Steven Heidel <steven@livingskyweb.ca>
2013-05-17 23:05:56 -0400

    Initial commit with bundler setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EncoreBackend::Application.config.secret_key_base = '50ce913734ca8910206363f367c9d1ed1d522a294b141481ca67f4c61062ee5aa46910f2f91b14b681f8ace7565a1c6e2d0a8e633168f0f12f7f9dde6e8dfcbf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EncoreBackend::Application.config.session_store :cookie_store, key: '_encore_backend_session'
