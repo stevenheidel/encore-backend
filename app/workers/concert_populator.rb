3f262a885cd86ac2f9b816afabf398ee6f51e2c7
Steven Heidel <steven@livingskyweb.ca>
2013-06-24 16:15:08 -0400

    Remember to pass in concert id to perform_at

--- app/workers/concert_populator.rb
+++ app/workers/concert_populator.rb
@@ -10,7 +10,7 @@ class ConcertPopulator
       ConcertPopulator.perform_at(concert.start_time, concert_id)
     else
       if Time.now < concert.end_time # concert is in progress
-        ConcertPopulator.perform_at(1.hour.from_now)
+        ConcertPopulator.perform_at(1.hour.from_now, concert_id)
       else # concert has passed
         concert.populated = true
         concert.save
