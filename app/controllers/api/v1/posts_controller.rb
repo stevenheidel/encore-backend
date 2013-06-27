322a3549f290a84874b77718cc443d27b10b3623
Steven Heidel <steven@livingskyweb.ca>
2013-06-27 11:05:08 -0400

    Return empty array if concert not on server when polling for posts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,19 +1,15 @@
 class Api::V1::PostsController < Api::V1::BaseController
   def index
-    @posts = Concert.find_by(songkick_uuid: params[:concert_id]).posts
+    @posts = Concert.find_by(songkick_uuid: params[:concert_id]).try(:posts)
   end
 
   # upload a user photo to the time_capsule
   def create
-    pp params
-
     @concert = Concert.find_by(songkick_uuid: params[:concert_id])
 
     @user_photo = @concert.user_photos.new
     @user_photo.photo = params[:image]
     @user_photo.save
-
-    pp @user_photo
 
     render 'api/v1/base/result.json', locals: {result: 'success'}
   end
