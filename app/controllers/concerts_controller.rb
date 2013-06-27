64dd457c0f8218672dda00f00f18985eb4e88e4b
Steven Heidel <steven@livingskyweb.ca>
2013-06-27 16:48:13 -0400

    Show page for concerts that aren't added to profile yet

--- app/controllers/concerts_controller.rb
+++ app/controllers/concerts_controller.rb
@@ -1,6 +1,11 @@
+require 'songkick_api'
+
 class ConcertsController < ApplicationController
   def show
-    @concert = Concert.find_by(songkick_uuid: params[:id])
-    @posts = @concert.posts
+    if @concert = Concert.find_by(songkick_uuid: params[:id])
+      @posts = @concert.posts
+    else
+      @concert = SongkickAPI.get_event_by_id(params[:id])
+    end
   end
 end
