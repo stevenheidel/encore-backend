2f0bce3ae3b05460a21e28c91db5c25ae5baddde
Steven Heidel <steven@livingskyweb.ca>
2013-07-22 15:52:01 -0400

    All features are passing now, closes #14

--- features/step_definitions/custom_api_steps.rb
+++ features/step_definitions/custom_api_steps.rb
@@ -8,6 +8,16 @@ Then(/^the JSON response should have (\d+) copies similar to this under "(.*?)":
   end
 end
 
+Then(/^the JSON response should have more than (\d+) copies similar to this under "(.*?)":$/) do |copies, json_path, json|
+  expected = JSON.parse(json)
+  results = JsonPath.new(json_path).on(JSON.parse(last_response.body))
+
+  results[0].length.should > copies.to_i
+  results[0].each do |result|
+    result.keys.should == expected.keys
+  end
+end
+
 Then(/^the JSON response should have something similar to the following under "(.*?)":$/) do |json_path, json|
   expected = JSON.parse(json)
   result = JsonPath.new(json_path).on(JSON.parse(last_response.body))
