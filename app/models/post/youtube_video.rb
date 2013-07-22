f8698a35ab2de5489b45e6a7b00d7d1c1d0b4216
Steven Heidel <steven@livingskyweb.ca>
2013-07-21 22:07:46 -0400

    Get the correct Youtube API link. Closes #13

--- app/models/post/youtube_video.rb
+++ app/models/post/youtube_video.rb
@@ -2,7 +2,7 @@ class Post::YoutubeVideo < Post
   def self.build_from_response(response)
     self.new(
       caption: response.title,
-      link: response.player_url,
+      link: response.media_content[0].url,
       image_url: response.thumbnails[2].url, # [2] is the hq default
       user_name: response.author.name
     )
