33f66a5a82ae386693fd40b6f1a445d2a492ef2f
Nate <nathan.rambarran@gmail.com>
2013-07-22 13:19:03 -0400

    fix for artist_spec

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -4,17 +4,18 @@ describe Artist, :vcr do
   let(:artist) { FactoryGirl.create :artist }
   let(:latitude) { 43.670906 }
   let(:longitude) { -79.393331 }
+  let(:geo) { Geo.new(latitude, longitude) }
 
   describe ".past_events" do
     it "should get past events from lastfm" do
-      artist.past_events(latitude, longitude).count.should == 1
+      artist.past_events(geo).count.should == 1
     end
 
     it "should get past events from database" do
-      lastfm_events = artist.past_events(latitude, longitude)
+      lastfm_events = artist.past_events(geo)
       Saver::Events.drain # run the cache creator
       artist.reload
-      database_events = artist.past_events(latitude, longitude)
+      database_events = artist.past_events(geo)
 
       lastfm_events.count.should == database_events.count
     end
