1e5c15725c5282f8680069a03395e710a1765b51
Steven Heidel <steven@livingskyweb.ca>
2013-07-22 11:12:17 -0400

    Remove old tests from events_spec

--- spec/api/v1/events_spec.rb
+++ spec/api/v1/events_spec.rb
@@ -1,32 +1,5 @@
 require 'spec_helper'
 
-describe "/api/v1/artists/:artist_id/events/past", type: :api, vcr: true do
-  let(:artist) { FactoryGirl.create :artist }
-  let(:url) { "/api/v1/artists/#{artist.lastfm_id}/events/past.json" }
-
-  it "should return past events" do
-    get url, {city: 'Toronto'}
-    last_response.body.should == "{\"events\":[{\"lastfm_id\":\"206280\",\"name\":\"Cher\",\"date\":\"2002-10-24\",\"image_url\":\"http://userserve-ak.last.fm/serve/252/71212358.png\",\"venue_name\":\"Air Canada Centre\"}]}"
-  end
-end
-
-describe "/api/v1/(artists/:artist_id/)events/future", type: :api, vcr: true do
-  let(:url_artist) { "/api/v1/artists/One+Direction/events/future.json" }
-  let(:url_city) { "/api/v1/events/future.json" }
-
-  it "should return future events (for an artist)" do
-    get url_artist, {city: 'Toronto'}
-
-    last_response.body
-  end
-
-  it "should return future events (for a city)" do
-    get url_city, {city: 'Toronto'}
-
-    last_response.body
-  end
-end
-
 describe "/api/v1/users/:facebook_id/events", type: :api, vcr: true do
   let(:event) { FactoryGirl.create :event }
   let(:user) { FactoryGirl.create :user }
