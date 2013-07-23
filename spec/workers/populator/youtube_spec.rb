90c4b29a47d2d0d5b744a0b998124e5c2aa1f5be
Steven Heidel <steven@livingskyweb.ca>
2013-07-23 17:02:26 -0400

    Unfinished exploration into Youtube playback restrictions

--- spec/workers/populator/youtube_spec.rb
+++ spec/workers/populator/youtube_spec.rb
@@ -5,14 +5,22 @@ describe Populator::Youtube, :vcr do
   let(:event2) { FactoryGirl.create :event,
     name: "Doobie Brothers", 
     start_date: "Oct 12, 2012"}
+  let(:event_with_not_for_mobile_youtubes) { FactoryGirl.create :event,
+    name: "Justin Bieber",
+    start_date: "December 18, 2010 19:06",
+    venue: FactoryGirl.create(:venue, city: "Miami")
+    }
 
   describe '.perform' do
-    before do
+    it 'should get some videos from Youtube' do
       Populator::Youtube.new.perform(event2.id)
+      Post::YoutubeVideo.count.should_not == 0
     end
 
-    it 'should get some videos from Youtube' do
-      Post::YoutubeVideo.count.should_not == 0
+    it 'should ignore unplayable videos' do
+      Populator::Youtube.new.perform(event_with_not_for_mobile_youtubes.id)
+      Post::YoutubeVideo.count # TODO example of youtube video that doesn't play:
+      # http://www.youtube.com/v/TwmQzCtErxQ?version=3&f=videos&d=Ab-9veXVzISEkHOrC_Mc4coO88HsQjpE1a8d1GxQnGDm&app=youtube_gdata
     end
   end
 end
