47a7479d6f6c4668e557552360c262b77e6c0ad5
Steven Heidel <steven@livingskyweb.ca>
2013-07-24 10:46:31 -0400

    Try another redis configuration

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,5 +1,12 @@
+# Set up Redis connection on Cloud 66
+if Rails.env.production?
+  redis_address = "redis://ENV['REDIS_ADDRESS']:6379/"
+else
+  redis_address = nil
+end
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV['REDIS_ADDRESS'] }
+  config.redis = { url: redis_address }
 
   config.server_middleware do |chain|
     chain.add Kiqstand::Middleware
@@ -7,5 +14,5 @@ Sidekiq.configure_server do |config|
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV['REDIS_ADDRESS'] }
+  config.redis = { url: redis_address }
 end
