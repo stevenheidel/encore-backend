c0a136b0cf9c6d9b3a69d38655fd773cc55bc7f6
Slavik Derevyanko <sderevyanko@mentel.com>
2013-08-23 16:43:54 -0400

    Added validations to Venue model. Validating presence of name and numericality of coordinates, if present.

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -13,6 +13,9 @@ class Venue
 
   index({coordinates: '2d'}, {min: -180, max: 180})
 
+  validates_presence_of :name
+  validate :validate_coordinates
+
   def latitude
     self.coordinates[1]
   end
@@ -27,5 +30,23 @@ class Venue
 
   def longitude=(num)
     self.coordinates[0] = num
+  end
+
+  private
+  def validate_coordinates
+    invalid_coordinates = []
+
+    if (self.coordinates[0].nil? and self.coordinates[1].present?) or
+       (self.coordinates[1].nil? and self.coordinates[0].present?)
+      invalid_coordinates = [true]
+    end
+
+    if(self.coordinates[0].present? and self.coordinates[1].present?)
+      invalid_coordinates = self.coordinates.map{ |coordinate|
+        coordinate.is_a? Numeric or
+        (coordinate.is_a? String and (coordinate.strip =~ /\A[-+]?[0-9]*\.?[0-9]+\Z/) == 0)
+      }.select{ |e| e == false }
+    end
+    errors.add(:coordinates, I18n.t('mongoid.errors.models.venue.attributes.coordinates')) unless invalid_coordinates.empty?
   end
 end
