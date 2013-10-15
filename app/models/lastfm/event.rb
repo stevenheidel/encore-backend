d6d1ca5a38f2dedbc5c7d1b2ffd9fd847041e2f8
Slavik Derevyanko <sderevyanko@mentel.com>
2013-10-15 13:14:19 -0400

    Whitelisted eventbrite URLs as ticket URLs

--- app/models/lastfm/event.rb
+++ app/models/lastfm/event.rb
@@ -61,7 +61,7 @@ class Lastfm::Event < Lastfm::Base
       urls.concat(@json["website"].to_s.scan(url_regexp))
       urls.concat(@json["tickets"].to_s.scan(url_regexp))
       if urls.present?
-        tickets_urls = urls.select{|url| url.match(/ticket|sale|buy|purchase/)}
+        tickets_urls = urls.select{|url| url.match(/ticket|sale|buy|purchase|eventbrite/)}
         @tickets_url = tickets_urls[0] if tickets_urls
       end
     end
