fb81429d0185c14138c4bb6416c484e4da141549
Steven Heidel <steven@livingskyweb.ca>
2014-01-05 21:13:19 -0600

    Try something to get rid of AR Timeout errors

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,8 +8,9 @@ before_fork do |server, worker|
     Process.kill 'QUIT', Process.pid
   end
 
-  defined?(ActiveRecord::Base) and
+  if defined?(ActiveRecord::Base)
     ActiveRecord::Base.connection.disconnect!
+  end
 end
 
 after_fork do |server, worker|
@@ -17,6 +18,10 @@ after_fork do |server, worker|
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  if defined?(ActiveRecord::Base)
+    config = Rails.application.config.database_configuration[Rails.env]
+    config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
+    config['pool']            = ENV['DB_POOL'] || 5
+    ActiveRecord::Base.establish_connection(config)
+  end
 end
