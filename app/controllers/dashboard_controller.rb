139c3024d8e6df610da01c048f37f3b47e37c608
Steven Heidel <steven@livingskyweb.ca>
2014-02-18 21:40:37 -0600

    Add statistics to the dashbaord

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,11 @@ class DashboardController < ActionController::Base
 
   def facebook_users
     @users = User.all.order("created_at DESC")
+
+    @num_users = @users.count
+    @num_events = @users.inject(0) { |mem, var| mem += var.events_count }
+
+    @events_per_user = @num_events.to_f / @num_users
+    @excluding_zero  = @num_events.to_f / User.where("events_count > ?", 0).count
   end
 end
