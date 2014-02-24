e6650c32dbcc57c48e5347f349ef7751309bb50f
Steven Heidel <steven@livingskyweb.ca>
2014-02-24 15:23:42 -0600

    Get load more posts working again and shuffle the posts

--- app/controllers/public/events_controller.rb
+++ app/controllers/public/events_controller.rb
@@ -3,6 +3,6 @@ require 'will_paginate/array'
 class Public::EventsController < Public::BaseController
   def show
     @event = Event.find_or_create_from_lastfm(params[:id])
-    @posts = @event.posts.paginate(:page => params[:page], :per_page => 21)
+    @posts = @event.posts.shuffle(random: Random.new(1)).paginate(:page => params[:page], :per_page => 21)
   end
 end
