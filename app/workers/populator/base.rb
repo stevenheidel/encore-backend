379b1eba6d4596322119bf2f290b6fac1b8dfc9f
Steven Heidel <steven@livingskyweb.ca>
2014-05-14 12:26:45 -0600

    Update base.rb
    Priority queue

--- app/workers/populator/base.rb
+++ app/workers/populator/base.rb
@@ -2,5 +2,5 @@ class Populator::Base
   include Sidekiq::Worker
   include Sidekiq::Status::Worker
 
-  sidekiq_options :queue => :default, :backtrace => false
+  sidekiq_options :queue => :priority, :backtrace => false
 end
